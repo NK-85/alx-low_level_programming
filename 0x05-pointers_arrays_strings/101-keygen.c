54bf4f38ae4f879926f4f9972e93d78eee2f220b
NK-85 <epkyeijnr@yahoo.co.uk>
2023-03-01 10:50:31 -0800

    edit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Lists all the natural numbers below 1024 (excluded)
- *        that are multiples of 3 or 5.
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, sum = 0;
+	int pass[100];
+	int i, sum, n;
 
-	for (i = 0; i < 1024; i++)
+	sum = 0;	
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-			sum += i;
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
-
-	printf("%d\n", sum);
 
 	return (0);
 }
+
