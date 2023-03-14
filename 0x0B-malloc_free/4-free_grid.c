8590b903d20e33990c8386745424cb1b08264f09
NK-85 <epkyeijnr@yahoo.co.uk>
2023-03-13 18:10:08 -0700

    edit

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
  * free_grid - frees the memory allocate for the grid
@@ -18,51 +18,4 @@ void free_grid(int **grid, int height)
 		free(grid[i]);
 
 	free(grid);
-}
-
-vi 100-argstostr.c
-#include <stdlib.h>
-#include "holberton.h"
-
-/**
-* *argstostr - concatenates all the arguments of the program
-* @ac: number of arguments
-* @av: array of arguments
-*
-* Return: Pointer to the new string (Success), NULL (Error)
-*/
-char *argstostr(int ac, char **av)
-{
-	int i, j, k, len;
-	char *str;
-
-	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
-	}
-
-	str = malloc(sizeof(char) * (len + 1));
-
-	if (str == NULL)
-		return (NULL);
-
-	k = 0;
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
-		str[k] = '\n';
-		k++;
-	}
-	
-	return (str);
 }
