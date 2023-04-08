fc17c0e99bc1cae7c13d7886e0f8d33ba9d9f5ef
NK-85 <epkyeijnr@gmail.com>
2023-04-08 03:45:26 -0700

    free listint safe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -4,6 +4,41 @@
 #include "lists.h"
 
 /**
+ * _ra - it reallocates memory for an array of pointers to the nodes in a linked list.
+ *
+ * @list: the old list to be be append.
+ * 
+ * @size: size of the new list (always one more than the old list).
+ *
+ * @new: new node to add.
+ *
+ * Return: pointer to the new list.
+ */
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **newlist;
+	size_t a;
+
+	newlist = malloc(size * sizeof(listint_t *));
+
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+
+	for (a = 0; a < size - 1; a++)
+
+		newlist[a] = list[a];
+
+	newlist[a] = new;
+
+	free(list);
+
+	return (newlist);
+}
+
+/**
  * free_listint_safe - a function that frees a listint_t linked list.
  *
  * @head: double pointer.
