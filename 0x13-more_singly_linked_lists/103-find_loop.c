5077124fb4ebadfa5757739d21efdb8dfdcaca81
NK-85 <epkyeijnr@gmail.com>
2023-04-08 04:12:17 -0700

    find the loops

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * find_listint_loop - function that  finds the loop in a linked list.
+ *
+ * @head: double pointer to the beginning of the list.
+ *
+ * Return: the address of the node where the loop starts or NULL if there's no loop.
+ *
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *snail, *hare;
+
+	snail = hare = head;
+
+	while (snail && hare && hare->next)
+	{
+		snail = tortoise->next;
+
+		hare = hare->next->next;
+
+		if (snail == hare)
+		{
+			snail = head;
+
+			break;
+		}
+	}
+
+	if (!snail || !hare || !hare->next)
+
+		return (NULL);
+
+	while (snail != hare)
+	{
+		snail = snail->next;
+
+		hare = hare->next;
+	}
+
+	return (hare);
+}
